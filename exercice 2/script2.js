5b945a3448af95e5230539185b8ac61e01f73162
dadurand <ha.halo@live.fr>
2019-09-16 12:17:36 +0200

    exercice 2

--- exercice 2/script2.js
+++ exercice 2/script2.js
@@ -1,4 +1,12 @@
-var linkElement = document.getElementsByName("a");
+var displayElement = document.getElementsByTagName("a")[0];
+var hideElement = document.getElementsByTagName("a")[1];
 var textElement = document.getElementById("texte");
 
 
+var displayInput = displayElement.addEventListener("click", function(){
+    textElement.style.display = "block" ;
+});
+
+var hideInput = hideElement.addEventListener("click", function(){
+    textElement.style.display = "none" ;
+});
