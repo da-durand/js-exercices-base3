1426b1b6a347324c39d6e5cd215fa4dcc221e6ee
Zatelbeeit <breaul@gmail.com>
2019-09-16 14:05:50 +0200

    exo 3 cont

--- exercice 3/script3.js
+++ exercice 3/script3.js
@@ -1,7 +1,9 @@
-var greenElement = document.getElementsByTagName("div")[1].getElementsByTagName("div")[0];
+var greenElement = document.getElementsByClassName("color green");
 var redElement = document.getElementsByClassName("color red");
 var blueElement = document.getElementsByClassName("color blue");
+var textElement = document.getElementById("texte");
 
 var greenInput = greenElement.addEventListener("click", function(){
-    
+    textElement.style.color = "red";
 });
+
