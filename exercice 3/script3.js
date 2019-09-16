1492af46054fcaad4bcc60ace46cd1e3dfce7c5a
dadurand <ha.halo@live.fr>
2019-09-16 13:38:20 +0200

    exercicie 3

---
+++ exercice 3/script3.js
@@ -0,0 +1,7 @@
+var greenElement = document.getElementsByTagName("div")[1].getElementsByTagName("div")[0];
+var redElement = document.getElementsByClassName("color red");
+var blueElement = document.getElementsByClassName("color blue");
+
+var greenInput = greenElement.addEventListener("click", function(){
+    
+});
