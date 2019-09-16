40c5ba5a977f7f3a78330c14a5b4d8bb9d1d2599
dadurand <ha.halo@live.fr>
2019-09-16 14:17:16 +0200

    fin exo 3

--- exercice 3/script3.js
+++ exercice 3/script3.js
@@ -1,9 +1,16 @@
-var greenElement = document.getElementsByClassName("color green");
-var redElement = document.getElementsByClassName("color red");
-var blueElement = document.getElementsByClassName("color blue");
+var greenElement = document.querySelector(".green");
+var redElement = document.querySelector(".red");
+var blueElement = document.querySelector(".blue");
 var textElement = document.getElementById("texte");
 
 var greenInput = greenElement.addEventListener("click", function(){
+    textElement.style.color = "green";
+});
+
+var redInput = redElement.addEventListener("click", function(){
     textElement.style.color = "red";
 });
 
+var blueInput = blueElement.addEventListener("click", function(){
+    textElement.style.color = "blue";
+});
